56f89fb9f94955fa00ad28add5d7e417d738a81f
Savita Katwe <savitamerwade04@gmail.com>
2024-03-30 22:13:51 +0400

    fixed test cases for responsify

--- src/helpers/responsify/responsify.test.ts
+++ src/helpers/responsify/responsify.test.ts
@@ -8,15 +8,11 @@ const bgColors = {
 
 describe('should test responsify', () => {
   it('should return result', () => {
-    expect(responsify('width', '100px')).toEqual({
-      width: '100px',
-    });
+    expect(responsify('width', '100px')).toEqual([['width', '100px']]);
   });
 
   it('should accept property as number', () => {
-    expect(responsify('flex-grow', 1)).toEqual({
-      'flex-grow': '1',
-    });
+    expect(responsify('flex-grow', 1)).toEqual([['flex-grow', 1]]);
   });
 
   it('should accept property for all devices', () => {
@@ -26,19 +22,25 @@ describe('should test responsify', () => {
         base: 1,
         md: 3,
       }),
-    ).toEqual({
-      '@media only screen and (max-width: 600px)': {
-        'flex-grow': '2',
-      },
-      '@media only screen and (min-width: 600px) and (max-width: 992px)': {
-        'flex-grow': '3',
-      },
-      'flex-grow': '1',
-    });
+    ).toEqual([
+      [
+        '@media only screen and (max-width: 600px)',
+        {
+          'flex-grow': 2,
+        },
+      ],
+      [
+        '@media only screen and (min-width: 600px) and (max-width: 992px)',
+        {
+          'flex-grow': 3,
+        },
+      ],
+      ['flex-grow', 1],
+    ]);
   });
 
   it('should return empty response', () => {
-    expect(responsify('flex-grow')).toEqual({});
+    expect(responsify('flex-grow')).toEqual([]);
   });
 
   it('should test responsive modify callback', () => {
@@ -52,22 +54,26 @@ describe('should test responsify', () => {
         },
         (property) => bgColors[property],
       ),
-    ).toEqual({
-      '@media only screen and (max-width: 600px)': {
-        'background-color': '#32d205',
-      },
-      '@media only screen and (min-width: 600px) and (max-width: 992px)': {
-        'background-color': '#161e80',
-      },
-      'background-color': '#ef0505',
-    });
+    ).toEqual([
+      [
+        '@media only screen and (max-width: 600px)',
+        {
+          'background-color': '#32d205',
+        },
+      ],
+      [
+        '@media only screen and (min-width: 600px) and (max-width: 992px)',
+        {
+          'background-color': '#161e80',
+        },
+      ],
+      ['background-color', '#ef0505'],
+    ]);
   });
 
   it('should test modify callback', () => {
     expect(
       responsify('background-color', 'red', (property) => bgColors[property]),
-    ).toEqual({
-      'background-color': '#ef0505',
-    });
+    ).toEqual([['background-color', '#ef0505']]);
   });
 });
