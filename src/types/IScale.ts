0b42724e8c57d111f5b5ebd021246c1a74d57f07
Savita Katwe <savitamerwade04@gmail.com>
2024-03-31 09:33:54 +0400

    Added responsive

--- src/types/IScale.ts
+++ src/types/IScale.ts
@@ -1,7 +1,8 @@
 import type { DefaultCrafter } from 'styled-crafter';
+import type IResponsiveValue from './IResponsiveValue';
 
 export type IRadii = keyof DefaultCrafter['radii'];
 export type IBorderThickness = keyof DefaultCrafter['borderThickness'];
 export type IColor = keyof DefaultCrafter['colors'];
-export type ISpacing = keyof DefaultCrafter['spacing'];
+export type ISpacing = IResponsiveValue<keyof DefaultCrafter['spacing']>;
 export type ISize = keyof DefaultCrafter['sizes'];
